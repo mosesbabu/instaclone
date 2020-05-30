04ce3424809d3b4bc9c748705c7417f2015226db
moselamar <44118179+moselamar@users.noreply.github.com>
2020-05-30 16:20:13 +0300

    uniitests oist

--- instaclone/tests.py
+++ instaclone/tests.py
@@ -1,45 +1,57 @@
 from django.test import TestCase
-from .models import Location,Profile,Post,Comment
-from django.contrib.auth.models import User
-import datetime as dt
-# Create your tests here.
-class LocationTestClass(TestCase):
-    def setUp(self):
-        self.Moringa = Location(location='Moringa')
 
-    def test_instance(self):
-        self.assertTrue(isinstance(self.Moringa,Location))
+# Create your tests here.
+from django.contrib.auth.models import User
+from user.models import Profile
+from django.test import TestCase
+from .models import Image
+class Intagram_TestCases(TestCase):
+    def setUp(self):
+        self.user1= User(id=1,username='Moses',email='moses@gmail.com',password='admin')
+        self.user1.save()
+        self.profile = Profile(bio='plucker',profile_path='image/image.jpg')
+        self.profile.save_profile()
+        self.new_image = Image(id=1,caption='learn', author='Mose',image_path='media/gallery/dance-3134828_1920.jpg',image_category=self.new_category,image_location=self.new_location)
+        self.new_image.save_image()
 
     def tearDown(self):
-        Location.objects.all().delete()
+        Profile.objects.all().delete()
+        User.objects.all().delete()
+        Image.objects.all().delete()
+
+    def test_is_instance(self):
+        self.assertTrue(isinstance(self.user1,User))
+        self.assertTrue(isinstance(self.profile,Profile))
+        self.assertTrue(isinstance(self.new_image,Image))
 
     def test_save_method(self):
-        self.Moringa.save_location()
-        locations = Location.objects.all()
-        self.assertTrue(len(locations)>0)
+        self.new_image.save_image()
+        all_objects = Image.objects.all()
+        self.assertTrue(len(all_objects)>0)
 
     def test_delete_method(self):
-        self.Moringa.delete_location('Moringa')
-        locations = Location.objects.all()
-        self.assertTrue(len(locations)==0)
+        self.new_image.save_image()
+        filtered_object = Image.objects.filter(author='Mose')
+        Image.delete_image(filtered_object)
+        all_objects = Image.objects.all()
+        self.assertTrue(len(all_objects) == 0)
 
-class CommentTestClass(TestCase):
-    def setUp(self):
-        self.new_user = User.objects.create_user(username='donald',password='password123')
-        self.comment = Comment(comment='Test Comment',username=self.new_user,post=1)
+    def test_display_all_objects_method(self):
+        self.new_image.save_image()
+        all_objects = Image.retrieve_all()
+        self.assertEqual(all_objects.author,'Mose')
 
-    def test_instance(self):
-        self.assertTrue(isinstance(self.comment,Comment))
 
-    def tearDown(self):
-        Comment.objects.all().delete()
-
-    def test_save_method(self):
-        self.comment.save_comment()
-        comments = Comment.objects.all()
-        self.assertTrue(len(comments)>0)
-
-    # def test_delete_method(self):
-    #     self.comment.delete_comment()
-    #     comments = Comment.objects.all()
-    #     self.assertTrue(len(comments)==0)
+    def test_update_single_object_property(self):
+        self.new_image.save_image()
+        filtered_object =Image.update_image('Mose','Babu')
+        fetched = Image.objects.get(author='Mose')
+        self.assertEqual(fetched.author,'Mose')
+    def test_get_image_by_id(self):
+        self.new_image.save_image()
+        fetched_image = Image.get_image_by_id(1)
+        self.assertEqual(fetched_image.id,1)
+    def test_search_by_username(self):
+        self.profile.save_profile()        
+        fetch_specific = Profile.objects.get(user=1)
+        self.assertTrue(fetch_specific.id==1)
